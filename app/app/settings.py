3b4212f4c613dd36880cb8edff7b50d23b22ad13
Pedro Andrade <pedrogamerp@gmail.com>
2024-08-03 20:10:10 -0300

    refactor: static and media file settings in app/settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -114,7 +114,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / 'static'
+else:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',
+    ]
+
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
