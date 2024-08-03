8e50aacf8dd8cd396a246020488b1bc7201e7766
Pedro Andrade <pedrogamerp@gmail.com>
2024-08-03 13:06:07 -0300

    refactor: Update User model to include username and profile image fields

--- app/user/models/user.py
+++ app/user/models/user.py
@@ -1,26 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin
+)
+from django.core.validators import MinLengthValidator
+from core.models import BaseModel
 
 
-class MyUserManager(BaseUserManager):
-    def create_user(self, email, date_of_birth, password=None):
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
         """
-        Creates and saves a User with the given email, date of
-        birth and password.
+        Creates and saves a User with the given email, username and password.
         """
         if not email:
-            raise ValueError("Users must have an email address")
+            raise ValueError("Usuários devem ter um endereço de e-mail")
+
+        if not username:
+            raise ValueError("Usuários devem ter um nome de usuário")
 
         user = self.model(
             email=self.normalize_email(email),
-            date_of_birth=date_of_birth,
+            username=username,
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, date_of_birth, password=None):
+    def create_superuser(self, email, username, password=None):
         """
         Creates and saves a superuser with the given email, date of
         birth and password.
@@ -28,43 +36,54 @@ class MyUserManager(BaseUserManager):
         user = self.create_user(
             email,
             password=password,
-            date_of_birth=date_of_birth,
+            username=username,
         )
         user.is_admin = True
         user.save(using=self._db)
         return user
 
 
-class MyUser(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin, BaseModel):
+    """
+    Custom user model with email as the unique identifier
+    """
     email = models.EmailField(
-        verbose_name="email address",
+        verbose_name="Endereço de e-mail",
         max_length=255,
         unique=True,
     )
-    date_of_birth = models.DateField()
+    username = models.CharField(
+        verbose_name="Nome de usuário",
+        max_length=255,
+        validators=[MinLengthValidator(3)],
+        unique=True
+    )
+    profile_img = models.ImageField(
+        upload_to='profile_images/', null=True, blank=True)
+    stations_to_notify = models.ManyToManyField(
+        'device.Device',
+        related_name='notified_users',
+        blank=True
+    )
+
+    notify_on_new_station = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
 
     objects = MyUserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["date_of_birth"]
+    REQUIRED_FIELDS = ["username"]
 
     def __str__(self):
+        """
+        String representation of the user
+        """
         return self.email
-
-    def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
 
     @property
     def is_staff(self):
-        "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
+        """
+        Is the user a member of staff?
+        """
         return self.is_admin
